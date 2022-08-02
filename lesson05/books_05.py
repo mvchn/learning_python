0fbfc2e31d88c7c1f31084c878566419ef577261
mvchn <movchan@gmail.com>
2022-08-02 17:51:50 +0300

    lesson05 tasks

---
+++ lesson05/books_05.py
@@ -0,0 +1,35 @@
+''' To write table with books:
+    - title (capitalize first letters)
+    - author (upper case)
+    - price (rounded)
+    Update01:
+        - add category
+        - add currency for price (use constant)
+        - remove code duplicates (use list)
+    Update02:
+        - use range for index in cycle
+        - remove space in constant CURR
+        - add index size to constants
+        - slice lists to the index size 
+        - remove code duplicates (if exists)   
+    Update03:
+        - increase final prices for python books by 30%
+        - reduce final prices for java books by 10%
+        - control lists size
+        - show the most popular book 
+        - remove code duplicates (if exists)      
+    Update04:        
+        + increase final prices for most popular book by 100%
+        + sell the most popular book 
+        + add more books
+        + add constant TAX_RATE
+        + calculate taxes 
+        - remove code duplicates (if exists) 
+    Update05:       
+        - remove unnecessary variables and constants
+        - desctibe functions by comments
+        - sell books, not popularity 
+        - add a new 3 books to the storage
+        - create a new sell cycle (2 sell cycles should be in the result)
+        
+'''
