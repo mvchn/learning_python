fccd2af8b0d5700ab2e9c01722335d0989277319
mvchn <movchan@gmail.com>
2022-10-01 16:31:12 +0300

    lesson 13 updates

---
+++ lesson13/lesson13.py
@@ -0,0 +1,24 @@
+# Landing
+
+data = {
+    "3222484500:22:444:8882": {
+        'land_id': "3222484500:22:444:8882",
+        'firstname': "Test",
+        'lastname': "Last",
+        'middlename': "Middle"
+    },
+    "3222484500:22:444:8884": {
+        'land_id': "3222484500:22:444:8884",
+        'firstname': "Test",
+        'lastname': "Last",
+        'middlename': "Middle"
+    },
+    "3222484500:22:444:8888": {
+        'land_id': "3222484500:22:444:8888",
+        'firstname': "Test",
+        'lastname': "Last",
+        'middlename': "Middle"
+    },
+}
+
+#TODO: append more items to the data
