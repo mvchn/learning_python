01f709b9ac88fa7cb1f3a80f6b83699e475e9883
mvchn <movchan@gmail.com>
2022-10-06 15:55:22 +0300

    added exits

--- number_guesser/number_guess.py
+++ number_guesser/number_guess.py
@@ -1,16 +1,25 @@
 from random import randint
+
+#TODO: remove duplicates
 n = randint(0, 100)
+
+#TODO: computer can generate 0 also
 answer = str(input('I thought of a number from 1 to 100. Do you want to guess?\n'))
 if 'yes' in answer or 'YES' in answer or 'Yes' in answer:
     print('OK. Input your number:')
 elif 'no' in answer or 'NO' in answer or 'No' in answer:
-    print ('OK. Bye!')#stop
+    print ('OK. Bye!')
+    exit()
 else:
-    print ("I'm sorry, but I don't understand you!")#stop
+    print ("I'm sorry, but I don't understand you!")
+    exit()
 
+#TODO: program fails on non-numerical input
 a = int(input())
+
 if a == n:
-    print ('Fantastic! Well done!!!')#stop
+    print ('Fantastic! Well done!!!')
+    exit()
 elif a > n:
     print ('Your number is a little more than my')
 elif a < n:
