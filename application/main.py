3a5c8285eec762befa0e0060241349ae0e6cd914
Roman Movchan <movchan@gmail.com>
2022-10-06 09:53:41 +0300

    fix id, correct dict (#47)

--- application/main.py
+++ application/main.py
@@ -21,21 +21,18 @@ except FileNotFoundError:
     print(f"File {filename} not found")
     exit()
 
-shelf = Shelf
+shelf = {}
 
 for line in lines:
-
     pieces = line.split(",")
     item = Book(pieces[0].strip(), pieces[1].strip(), pieces[2].strip(), pieces[3].strip(), pieces[4].strip(), pieces[5].strip())
-    # TODO: fix line
-    #shelf.add_book(item)
+    shelf[pieces[0].strip()] = item
+
     print(f"{item.get_id()}. {item.get_name()}")
 
 
 id = input('Enter id: ')
 
-item.print_book()
+result = shelf.get(id)
 
-# event = MarketEvent(id, '30.08.2022', 10, CURR)
-#
-# event.print_event()
+result.print_book()
