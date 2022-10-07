c099be54891329bcfdf21188874ac5772f9f54ce
Bogdan <fedyna21@gmail.com>
2022-10-07 08:09:46 +0300

    Issue 49. added constant, comp can generate 0

--- number_guesser/number_guess.py
+++ number_guesser/number_guess.py
@@ -1,28 +1,32 @@
 from random import randint
 
-#TODO: remove duplicates
-n = randint(0, 100)
+LN = 50  # LN - largest number of range
+n = randint(0, LN)
 
-#TODO: computer can generate 0 also
-answer = str(input('I thought of a number from 1 to 100. Do you want to guess?\n'))
+# TODO: computer can generate 0 also
+answer = str(input("I thought of a number from 0 to " + str(LN) + ". Do you want to guess?\n"))
 if 'yes' in answer or 'YES' in answer or 'Yes' in answer:
     print('OK. Input your number:')
 elif 'no' in answer or 'NO' in answer or 'No' in answer:
-    print ('OK. Bye!')
+    print('OK. Bye!')
     exit()
 else:
-    print ("I'm sorry, but I don't understand you!")
+    print("I'm sorry, but I don't understand you!")
     exit()
 
-#TODO: program fails on non-numerical input
+# TODO: program fails on non-numerical input
 a = int(input())
 
-if a == n:
-    print ('Fantastic! Well done!!!')
+try:
+    if a == n:
+        print('Fantastic! Well done!!!')
+        exit()
+    elif a > n:
+        print('Your number is a little more than my')
+    elif a < n:
+        print('My number is a little more than yours')
+except ValueError:
+    print('\nThis is not a number. Try again...')
     exit()
-elif a > n:
-    print ('Your number is a little more than my')
-elif a < n:
-    print('My number is a little more than yours')
 
 print(n)
