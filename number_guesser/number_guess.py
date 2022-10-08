59c1b2418a76d0ac7af8cb166897f80d39bd37c9
Bogdan <fedyna21@gmail.com>
2022-10-08 15:11:55 +0300

    Issue 49. added constant SN,

--- number_guesser/number_guess.py
+++ number_guesser/number_guess.py
@@ -1,12 +1,11 @@
 from random import randint
-
+SN = 0   # SN - smallest number of range
 LN = 50  # LN - largest number of range
-n = randint(0, LN)
+n = randint(SN, LN)
 
-# TODO: computer can generate 0 also
-answer = str(input("I thought of a number from 0 to " + str(LN) + ". Do you want to guess?\n"))
+answer = str(input("I'm thinking of a number from " + str(SN) + " to " + str(LN) + ". Do you want to guess?\n"))
 if 'yes' in answer or 'YES' in answer or 'Yes' in answer:
-    print('OK. Input your number:')
+    print('OK!')
 elif 'no' in answer or 'NO' in answer or 'No' in answer:
     print('OK. Bye!')
     exit()
@@ -14,9 +13,23 @@ else:
     print("I'm sorry, but I don't understand you!")
     exit()
 
-# TODO: program fails on non-numerical input
-a = int(input())
+a = int(input('Input your number:'))
+if type(a) == int:
+    if SN <= a <= LN:
+        if a == n:
+            print('Fantastic! Well done!!!')
+            exit()
+        elif a > n:
+            print('Your number is a little more than mine')
+        elif a < n:
+            print('Your number is a little less than mine')
+    else:
+        print("\nPlease, input a number from " + str(SN) + " to " + str(LN))
+        exit()
+else:
+    print('The variable is not a number')
 
+print(n)
 try:
     if a == n:
         print('Fantastic! Well done!!!')
@@ -27,6 +40,4 @@ try:
         print('My number is a little more than yours')
 except ValueError:
     print('\nThis is not a number. Try again...')
-    exit()
-
-print(n)
+print (a + n)
