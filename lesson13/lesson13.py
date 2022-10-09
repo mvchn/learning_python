af5777f3cf1a065419a70cb9f343e32057f75616
Bogdan <fedyna21@gmail.com>
2022-10-09 15:31:00 +0300

    Issue 46. added more items to the data.

--- lesson13/lesson13.py
+++ lesson13/lesson13.py
@@ -3,22 +3,33 @@
 data = {
     "3222484500:22:444:8882": {
         'land_id': "3222484500:22:444:8882",
+        "land's location": "Village, street",
         'firstname': "Test",
         'lastname': "Last",
-        'middlename': "Middle"
+        'middlename': "Middle",
+        'the identification number of the owner': "1234567890",
+        "owner's address": "Owner's address",
+        'phone number': "+380671234567"
     },
     "3222484500:22:444:8884": {
         'land_id': "3222484500:22:444:8884",
+        "land's location": "Village, street",
         'firstname': "Test",
         'lastname': "Last",
-        'middlename': "Middle"
+        'middlename': "Middle",
+        'the identification number of the owner': "1234567890",
+        "owner's address": "Owner's address",
+        'phone number': "+380671234567"
     },
     "3222484500:22:444:8888": {
         'land_id': "3222484500:22:444:8888",
+        "land's location": "Village, street",
         'firstname': "Test",
         'lastname': "Last",
-        'middlename': "Middle"
+        'middlename': "Middle",
+        'the identification number of the owner': "1234567890",
+        "owner's address": "Owner's address",
+        'phone number': "+380671234567"
     },
 }
 
-#TODO: append more items to the data
