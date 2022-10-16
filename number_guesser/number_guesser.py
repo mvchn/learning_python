fec0b9a983c21168eb9cdb8b1bd1e87735022d7b
Roman Movchan <movchan@gmail.com>
2022-10-16 15:08:38 +0300

    lesson18 updates (#68)

--- number_guesser/number_guesser.py
+++ number_guesser/number_guesser.py
@@ -1,19 +1,31 @@
 from random import randint
-n = randint(0, 100)
-answer = str(input('I thought of a number from 1 to 100. Do you want to guess?\n'))
-if 'yes' in answer or 'YES' in answer or 'Yes' in answer:
-    print('OK. Input your number:')
-elif 'no' in answer or 'NO' in answer or 'No' in answer:
-    print ('OK. Bye!')#stop
-else:
-    print ("I'm sorry, but I don't understand you!")#stop
 
-a = int(input())
-if a == n:
-    print ('Fantastic! Well done!!!')#stop
-elif a > n:
-    print ('Your number is a little more than my')
-elif a < n:
-    print('My number is a little more than yours')
 
-print(n)
+def hello_casino():
+    answer = str(input('I thought of a number from 1 to 100. Do you want to guess?\n'))
+    if 'yes' in answer or 'YES' in answer or 'Yes' in answer:
+        print('OK. Input your number:')
+    elif 'no' in answer or 'NO' in answer or 'No' in answer:
+        print('OK. Bye!')
+    else:
+        print("I'm sorry, but I don't understand you!")
+
+
+def choice(c, u):
+    if u == c:
+        return '*** Fantastic! Well done!!! ***'
+    elif u > c:
+        return 'Your number is a little more'
+    elif u < c:
+        return 'Your number is a little less'
+
+
+def main():
+    hello_casino()
+    computer = randint(0, 100)
+    user = int(input())
+    print(choice(computer, user))
+
+
+if __name__ == '__main__':
+    main()
