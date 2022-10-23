08b35c0d8bf71b7252a3d7dc3f6703b7a5eed79e
Roman Movchan <movchan@gmail.com>
2022-10-23 18:15:10 +0300

    lesson20 prepare (#73)

---
+++ lesson20/lesson20.py
@@ -0,0 +1,10 @@
+start = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22]
+
+print(start)
+
+# TODO: find index of value 10 and print if exists
+# TODO: shuffle list
+# TODO: find index of value 12 and print if exists
+# TODO: remove two random items
+# TODO: sort items
+
