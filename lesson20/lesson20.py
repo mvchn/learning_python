efbaeed672fb0d201875ed269ebdb5a0c92b000e
Bogdan <fedyna21@gmail.com>
2022-10-31 05:59:55 +0200

    Issue 75. made corrections

--- lesson20/lesson20.py
+++ lesson20/lesson20.py
@@ -1,16 +1,15 @@
 import random
+
 start = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22]
 
 print(start)
 
-print (start[10])
+print(start.index(10))
 random.shuffle(start)
-print (start[12])
+print(start.index(12))
 random_item = random.choice(start)
 start.remove(random_item)
 random_item_2 = random.choice(start)
 start.remove(random_item_2)
-start.sort
-print(start)
-
-
+sorted_list = sorted(start)
+print(sorted_list)
