e832a0b2013aca6dde83d51f9384e88cdcdb5a18
Roman Movchan <movchan@gmail.com>
2022-11-13 17:11:11 +0200

    recursive function fibbo (#93)

--- fibbo/fibbo.py
+++ fibbo/fibbo.py
@@ -1,4 +1,16 @@
+def fibbo(i):
+    if i == 0:
+        return 0
+    elif i == 1:
+        return 1
+    else:
+        return fibbo(i-1) + fibbo(i-2)
+
+
 num = int(input("Enter range border:"))
+print('---------')
+print(fibbo(num))
+print('---------')
 f1 = 0
 f2 = 1
 for i in range(1, num):
@@ -6,4 +18,4 @@ for i in range(1, num):
         f1, f2 = f2, f1 + f2
     else:
         break
-    print (f2)
+    print(f2)
