478aa830e76b9eb7506b4b199006e42479ab9f51
mvchn <movchan@gmail.com>
2022-11-20 16:08:44 +0200

    menu updates

--- menu.py
+++ menu.py
@@ -1,2 +1,7 @@
 print(f"[{1}]. installation(check_requirements)")
-print(f"[{2}]. print('Hello World') ")
+print(f"[{2}]. print('Hello World')")
+print(f"[{3}]. variables, math operations")
+print(f"[{4}]. user input, format output")
+# TODO: list, tuple
+choice = input("Enter your choice [1-3]: ")
+print("Your choice is", choice)
