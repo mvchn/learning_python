d382cbcc457a131b3ac0beefad17599f4cea0f55
Bogdan <fedyna21@gmail.com>
2022-11-21 05:53:39 +0200

    fizz_buzz_2

--- Fizz_buzz/fizz_buzz.py
+++ Fizz_buzz/fizz_buzz.py
@@ -1,12 +1,13 @@
-for i in range (1,102):
-    if i % 5 == 0 and i % 3 == 0:
-        print ('fizz_buzz')
-    if i % 5 == 0:
-        print('buzz')
-    if i % 3 == 0:
-        print ('fizz')
+nums = list(range(1,  102))
+for num in nums:
+    if num % 5 == 0 and num % 3 == 0:
+        print(num, '- fizz buzz')
+    elif num % 5 == 0:
+        print(num, '- buzz')
+    elif num % 3 == 0:
+        print(num, '- fizz')
     else:
-        if i % 2 == 0:
-            print ('odd')
-        if i % 2 != 0:
-            print ('even')
+        if num % 2 != 0:
+            print(num, '- odd')
+        if num % 2 == 0:
+            print(num, '- even')
