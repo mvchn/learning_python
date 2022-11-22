617a54e7d817f1c9cda3330b2e6389cec3d5d248
mvchn <movchan@gmail.com>
2022-11-22 19:14:23 +0200

    add list items

--- menu.py
+++ menu.py
@@ -1,7 +1,27 @@
-print(f"[{1}]. installation(check_requirements)")
-print(f"[{2}]. print('Hello World')")
-print(f"[{3}]. variables, math operations")
-print(f"[{4}]. user input, format output")
-# TODO: list, tuple
+print(f"[{0}]. installation(check_requirements)")
+
+print(f"[{1}]. first program")
+print(f"[{2}]. variables, math operations")
+
+var = 'name'
+
+print(var[0])
+print(var[1])
+print(var[2])
+print(var[3])
+
+print(f"[{3}]. list")
+
+for i in range(0, 4):
+    print(var[i])
+
+print(f"[{3}]. for")
+print(f"[{4}]. if else ")
+
+print(f"[{5}]. user input, format output")
+
+print(f"[{6}]. user input, format output")
+
+# TODO: tuple
 choice = input("Enter your choice [1-3]: ")
 print("Your choice is", choice)
