2d03917fab946a46514bfc62323753166bb0cb9a
mvchn <movchan@gmail.com>
2022-12-04 22:06:27 +0200

    extended menu

--- menu.py
+++ menu.py
@@ -1,6 +1,9 @@
 print(f"[{0}]. installation(check_requirements)")
 
 print(f"[{1}]. first program")
+
+print('hello, python developers')
+
 print(f"[{2}]. variables, math operations")
 
 var = 'name'
@@ -10,18 +13,17 @@ print(var[1])
 print(var[2])
 print(var[3])
 
-print(f"[{3}]. list")
+print(f"[{3}]. list of authors, actors, events")
 
 for i in range(0, 4):
     print(var[i])
 
-print(f"[{3}]. for")
-print(f"[{4}]. if else ")
+print(f"[{3}]. for, while parameters (weight, amount)")
+
+print(f"[{4}]. if markers else tags")
 
 print(f"[{5}]. user input, format output")
 
-print(f"[{6}]. user input, format output")
+choice = input(f"Enter your choice [{1}-{5}]: ")  # Choose your option
 
-# TODO: tuple
-choice = input("Enter your choice [1-3]: ")
 print("Your choice is", choice)
