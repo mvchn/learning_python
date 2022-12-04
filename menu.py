8e0f89604142be13b67fdade6ab51160e6630e68
mvchn <movchan@gmail.com>
2022-12-04 22:13:33 +0200

    clean menu

--- menu.py
+++ menu.py
@@ -1,29 +1,8 @@
-print(f"[{0}]. installation(check_requirements)")
-
-print(f"[{1}]. first program")
-
-print('hello, python developers')
-
-print(f"[{2}]. variables, math operations")
-
-var = 'name'
-
-print(var[0])
-print(var[1])
-print(var[2])
-print(var[3])
-
-print(f"[{3}]. list of authors, actors, events")
-
-for i in range(0, 4):
-    print(var[i])
-
-print(f"[{3}]. for, while parameters (weight, amount)")
-
-print(f"[{4}]. if markers else tags")
-
-print(f"[{5}]. user input, format output")
-
-choice = input(f"Enter your choice [{1}-{5}]: ")  # Choose your option
-
-print("Your choice is", choice)
+print("[1]. installation (check requirements)")
+print("[2]. first program")
+print("[3]. variables, math operations")
+# comments
+print("[4]. list of authors, actors, events")
+print("[5]. for, while parameters (weight, amount)")
+print("[6]. if markers else tags")
+print("[7]. user input, format output")
