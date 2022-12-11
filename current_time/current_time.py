82de559652d6d350f6b412654eeac2ab9f6746b4
Roman Movchan <movchan@gmail.com>
2022-12-11 17:39:53 +0200

    Issues/100 (#106)

--- current_time/current_time.py
+++ current_time/current_time.py
@@ -2,9 +2,11 @@ from datetime import datetime
 
 now = datetime.now()
 current_minute = int(now.strftime("%M"))
-if current_minute % 2 != 0:
+odd_minutes = list(range(1, 60, 2))
+if current_minute in odd_minutes:
     print('YES! Current minute is odd')
 else:
     print("NO! Current minute is even")
 
 print(current_minute)
+
