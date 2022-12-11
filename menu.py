f0b772ae24aa91a1674f627cb7d3bd9b091bb1be
Roman Movchan <movchan@gmail.com>
2022-12-11 17:29:10 +0200

    menu items (#94)

---
+++ menu.py
@@ -0,0 +1,8 @@
+print("[1]. installation (check requirements)")
+print("[2]. first program")
+print("[3]. variables, math operations")
+# comments
+print("[4]. list of authors, actors, events")
+print("[5]. for, while parameters (weight, amount)")
+print("[6]. if markers else tags")
+print("[7]. user input, format output")
